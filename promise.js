558d95d9786cb8bfcccf0b9a80dff37ddbeb0135
marurei <ahmad.syarif.sy@gmail.com>
2022-09-16 20:32:13 +0700

    1st attempt

--- promise.js
+++ promise.js
@@ -1,8 +1,26 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+// const promiseOutput = null;
 
+async function promiseOutput(hasil2) {
+  const result = await promiseTheaterIXX();
+  const result2 = await promiseTheaterVGC();
+
+  let i = 0;
+  for (let index = 0; index < result.length; index++) {
+    if (result[index].hasil == hasil2) {
+      i ++;
+    }
+  }
+  for (let index = 0; index < result2.length; index++) {
+    if (result2[index].hasil == hasil2) {
+      i ++;
+    }
+  }
+
+  return i
+}
 module.exports = {
   promiseOutput,
 };
